d0e9a663a5340694236351ca82e884fdc76574ea
galvarinense <123342131+galvarinense@users.noreply.github.com>
2024-09-22 12:58:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -125,3 +125,11 @@ function buscar() {
             }
         });
     }
+    function toggleMenu(){
+        var menu = document.getElementById("menu");
+        if (menu.className === "menu"){
+            menu.className += " responsive";
+        } else {
+            menu.className = "menu";
+        }
+    }
